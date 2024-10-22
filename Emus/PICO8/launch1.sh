5231cdbad67511fa30428ddebf2f6a20445a9171
Ninoh-FOX <ninoh-fox@gmail.com>
2024-10-22 11:42:16 +0200

    added pico8 standalone launcher in emulator secction too

---
+++ Emus/PICO8/launch1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+EMU_DIR="/mnt/SDCARD/Emus/PICO8"
+GAMEDIR="/mnt/SDCARD/Ports/Pico8"
+
+$EMU_DIR/cpufreq.sh
+$EMU_DIR/cpuswitch1.sh
+
+HOME="$GAMEDIR"
+
+cd $HOME
+
+LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH"
+PATH="$HOME"/bin:$PATH
+
+
+pico8_64 -run "$1" 2>&1 | tee $HOME/log.txt
--- Ports/Pico8-fullscreen/launch.sh
+++ Ports/Pico8-fullscreen/launch.sh
@@ -9,5 +9,11 @@ cd $HOME
 LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH"
 PATH="$HOME"/bin:$PATH
 
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
 
 pico8_64 -splore -draw_rect 0,0,1024,768 2>&1 | tee $HOME/log.txt
